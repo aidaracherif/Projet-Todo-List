589b752bf1ca9b4e6f841d516a713fe4e5f3eb64
aidaracherif <md.aidara3@isepdiamniadio.edu.sn>
2022-11-25 21:42:51 +0000

    changement

--- main.js
+++ main.js
@@ -1,13 +1,44 @@
-const searh = document.querySelector(".search .textarea"),
+const add= document.querySelector(".add .textarea1"),
    todoLists = document.querySelector(".todoLists"),
    pendingNum = document.querySelector(".pending-num"),
-   clearButton = document.querySelector(".clear-button");
+   btn = document.querySelector(".btn");
+    
+   function allTasks(){
+    let tasks = document.querySelector(".pending");
+   }
 
-   searh.addEventListener("click" , (e) => {
-let inputVal = searh.value.trim();
+   add.addEventListener("keyup" , (e) => {
+    let inputVal = add.value.trim();
+    ;
 
-    if(e.key === "Enter" && inputVal > 0){
-        console.log(inputVal);
 
+       if (e.key === "Enter" && inputVal.length > 0){
+          let litag = `<li class="list pending" onclick="handleStatus(this)">
+          <input type="checkbox">
+          <span class="task">${inputVal}</span>
+          <i class="uil uil-trash-alt" onclick= "deleteTask(this)"></i>
+          </li>`;
+
+          todoLists.insertAdjacentHTML("beforeend", litag);
+          add.value = "";
+          allTasks();
+       }
+    });
+      
+    function deleteTask(e){
+        e.parentElement.remove(); 
     }
-})
+
+    function handleStatus(e){
+        const checkbox = e.querySelector("input");
+        console.log(checkbox)
+        checkbox.checked = checkbox.checked ? false : true;
+        e.classList.toggle("pending")
+    } 
+
+    btn.addEventListener("click" , () => {
+        todoLists.innerHTML = "";
+    })
+      
+
+    
